1129b7c081e85d0ad1a4f92421c05fbeebc2e590
Harsh C. Srivastava <harshcsrivastava@gmail.com>
2026-09-16 22:12:28 +0530

    feat: Project Initialization and Shadcn Setup

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/health", (req, res) => {
+  res.send("Server is healthy!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}); 
