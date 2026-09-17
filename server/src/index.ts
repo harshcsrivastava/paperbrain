49cf0d15519218c00f6fed8e41fd7b5e38876600
Harsh C. Srivastava <harshcsrivastava@gmail.com>
2026-09-17 16:36:48 +0530

    feat: add BetterAuth authentication with Google and middleware to intercept.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
