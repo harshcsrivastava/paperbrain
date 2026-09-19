01a91e3d6fab69efcf36a33a85ec7653047ad571
Harsh C. Srivastava <harshcsrivastava@gmail.com>
2026-09-19 20:37:21 +0530

    feat: add workspaces backend routes, controller, services and repository in express.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,20 @@ import express from "express";
 import "dotenv/config";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import cors from "cors";
+import { registerRoutes } from "./routes/index.js";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT;
+const clientUrl = process.env.CLIENT_URL ?? "http://localhost:3000";
+
+app.use(
+  cors({
+    origin: clientUrl,
+    credentials: true,
+  }),
+);
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 
@@ -17,6 +28,9 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
   res.send("Server is healthy!");
 });
+
+registerRoutes(app);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
